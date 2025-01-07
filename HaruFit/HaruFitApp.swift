e2a14b53b3590c836830a11330d139f0a1d234f0
SijongKim93 <rlatlwhd456@gmail.com>
2025-01-07 23:33:45 +0900

    [Setting] git ignore, swift format 추가

---
+++ HaruFit/HaruFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaruFitApp.swift
+//  HaruFit
+//
+//  Created by 김시종 on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HaruFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
